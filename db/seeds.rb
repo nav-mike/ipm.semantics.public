2bbdafbd00f59887919629fa45c6c09fad5219fa
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-01-30 17:51:14 +0300

    config devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create email: 'admin@admin.com', password: '12345678'
+admin.add_role :admin
