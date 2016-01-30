a7b8f8e37c7a714004d12d1ec1ec77eb9c0f99ef
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-01-30 17:21:32 +0300

    config generators

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,24 @@ module SemanticCmsDemo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    
+    config.time_zone = 'Moscow'
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Config gem
+    Bundler.require(*Rails.groups)
+    Config::Integration::Rails::Railtie.preload
+
+    config.generators do |g|
+      g.orm :active_record
+      g.test_framework :rspec
+      g.stylesheets true
+      g.javascripts true
+      g.jbuilder true
+      g.assets false
+      g.helper false
+    end
+
   end
 end
