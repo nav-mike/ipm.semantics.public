5e230eda10e952a494cbf4f8f5917bba0ab5d8d9
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-01-31 18:14:01 +0300

    add people seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+STDOUT.write 'create users'
 admin = User.create email: 'admin@admin.com', password: '12345678'
 admin.add_role :admin
 
 # Create projects
+STDOUT.write "\ncreate projects"
 
 5.times do
   Project.create! name: Faker::Lorem.word,
@@ -25,6 +27,17 @@ admin.add_role :admin
                   link: Faker::Internet.url
 end
 
+STDOUT.write "\ncreate publications"
+
 17.times do
   Publication.create! bib: "#{Faker::Name.name}, #{Faker::Name.name}, #{Faker::Name.name} #{Faker::Lorem.sentence} // #{Faker::Lorem.sentence(2)} #{[*1991..2016].sample}. V. #{Faker::Number.number(2)}. N #{Faker::Number.number(1)}."
+end
+
+STDOUT.write "\ncreate people"
+
+12.times do
+  Person.create! name: Faker::Name.name,
+                 university: 'ITMO University (Saint Petersburg National Research University of Information Technologies, Mechanics and Optics)',
+                 email: Faker::Internet.email,
+                 photo: Faker::Avatar.image
 end
