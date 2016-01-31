70cd8b44765b2c67a5ad5a6260f756ade8e9e37a
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2016-01-31 15:21:27 +0300

    add publication html

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -69,6 +69,6 @@ class PublicationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def publication_params
-      params[:publication]
+      params.require(:publication).permit(:bib)
     end
 end
